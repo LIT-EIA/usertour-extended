7c92e9586282752bd730e49be1ec34babcf6703c
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2025-10-14 12:29:26 -0400

    test

--- src/bookmarklet/bookmarklet.js
+++ src/bookmarklet/bookmarklet.js
@@ -3,6 +3,7 @@
   if (document.querySelector('.custom-modal-overlay')) {
     return;
   }
+  
   // Persistent data
   function loadStorage() {
     var local = localStorage.getItem("ut-configurator");
