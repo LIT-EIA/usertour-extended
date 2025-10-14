3af06e0a2a48cf9a390a7f8c2a8f5c95d2bd3f35
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2025-10-14 14:47:27 -0400

    test

--- src/library/usertour.extended.js
+++ src/library/usertour.extended.js
@@ -1,61 +1,63 @@
-(() => {
-  const STORAGE_KEY = 'ut-configurator';
-  
-  const loadStorageProp = (prop) => {
-    const local = localStorage.getItem(STORAGE_KEY);
-    const storage = JSON.parse(local) || {};
+(function () {
+  var STORAGE_KEY = 'ut-configurator';
+
+  function loadStorageProp(prop) {
+    var local = localStorage.getItem(STORAGE_KEY);
+    var storage = JSON.parse(local) || {};
     return storage && storage[prop] ? storage[prop] : null;
-  };
+  }
 
-  const enableTestMode = () => { 
-    const testMode = loadStorageProp('testMode');
+  function enableTestMode() {
+    var testMode = loadStorageProp('testMode');
 
-    const existingBanner = document.querySelector('.vertical-banner');
+    var existingBanner = document.querySelector('.vertical-banner');
     if (existingBanner) existingBanner.remove();
 
-    const existingStyle = document.querySelector('style[data-testmode-style]');
+    var existingStyle = document.querySelector('style[data-testmode-style]');
     if (existingStyle) existingStyle.remove();
 
     if (testMode) {
-      const banner = document.createElement('div');
+      var banner = document.createElement('div');
       banner.className = 'vertical-banner';
       banner.innerHTML = '<span class="vertical-text">Usertour Mode: Test</span>';
 
-      const style = document.createElement('style');
+      var style = document.createElement('style');
       style.setAttribute('data-testmode-style', 'true');
-      style.textContent = `
-        body {
-          padding-left: 15px!important;
-        }
-        .vertical-banner {
-          width: 15px;
-          height: 100%;
-          background-color: rgb(255, 0, 90);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          position: fixed;
-          top: 0;
-          left: 0;
-          z-index: 9999;
-        }
-        .vertical-text {
-          writing-mode: vertical-lr;
-          text-orientation: mixed;
-          color: white;
-          font-size: 12px;
-          font-weight: bold;
-        }
-      `;
+      style.textContent =
+        'body {' +
+        '  padding-left: 15px!important;' +
+        '}' +
+        '.vertical-banner {' +
+        '  width: 15px;' +
+        '  height: 100%;' +
+        '  background-color: rgb(255, 0, 90);' +
+        '  display: flex;' +
+        '  align-items: center;' +
+        '  justify-content: center;' +
+        '  position: fixed;' +
+        '  top: 0;' +
+        '  left: 0;' +
+        '  z-index: 9999;' +
+        '}' +
+        '.vertical-text {' +
+        '  writing-mode: vertical-lr;' +
+        '  text-orientation: mixed;' +
+        '  color: white;' +
+        '  font-size: 12px;' +
+        '  font-weight: bold;' +
+        '}';
 
       document.body.appendChild(style);
       document.body.appendChild(banner);
     }
-  };
+  }
 
-  const waitForUsertour = (callback, interval = 100, timeout = 5000) => {
-    const start = Date.now();
-    const check = () => {
+  function waitForUsertour(callback, interval, timeout) {
+    interval = interval || 100;
+    timeout = timeout || 5000;
+    var start = Date.now();
+
+    function check() {
       if (window.usertour && typeof window.usertour.init === 'function') {
         callback();
       } else if (Date.now() - start < timeout) {
@@ -63,22 +65,28 @@
       } else {
         console.warn('Usertour object not ready after timeout');
       }
-    };
-    check();
-  };
+    }
 
-  waitForUsertour(() => {
-    usertour.enableUserTour = (userData) => {
-      const environment = loadStorageProp('testMode')
+    check();
+  }
+
+  waitForUsertour(function () {
+    usertour.enableUserTour = function (userData) {
+      var environment = loadStorageProp('testMode')
         ? window.USERTOURJS_ENV_VARS.ENVIRONMENT_TEST
         : window.USERTOURJS_ENV_VARS.ENVIRONMENT_LIVE;
 
       usertour.init(environment);
-      const role = userData?.role
-        || usertour._app?.userInfo?.data?.role
-        || null;
 
-      usertour.identifyAnonymous({ role });
+      var role =
+        (userData && userData.role) ||
+        (usertour._app &&
+          usertour._app.userInfo &&
+          usertour._app.userInfo.data &&
+          usertour._app.userInfo.data.role) ||
+        null;
+
+      usertour.identifyAnonymous({ role: role });
       enableTestMode();
     };
   });
